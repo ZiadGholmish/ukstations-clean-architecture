ef217f168ab2d2f5481bd79658daecda826b0ec2
Carriage Co <ziadgholmish@gmail.com>
2018-07-24 19:53:02 +0300

    Add leak canary lib and enable multi dex

--- app/src/main/java/com/citymapper/app/app/CitymapperApp.kt
+++ app/src/main/java/com/citymapper/app/app/CitymapperApp.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import com.citymapper.app.dagger.AppComponent
 import com.citymapper.app.dagger.AppModule
 import com.citymapper.app.dagger.DaggerAppComponent
+import com.squareup.leakcanary.LeakCanary
 
 class CitymapperApp : Application() {
 
@@ -11,6 +12,12 @@ class CitymapperApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return
+        }
+        LeakCanary.install(this);
         initializeInjector()
     }
 
